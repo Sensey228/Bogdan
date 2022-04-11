c204dcb84d6cabd6da494bdddd0e187f71795cf3
Bogdan132 <39145828+Sensey228@users.noreply.github.com>
2022-04-11 01:20:14 +0000

    Слайсинг(пример)

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
+nums = [n for n in range(20)]
+print(nums)
+print(nums[2:5])
+name = 'Python is not a snake'
+print(name[::-1])
 
-# 0 — 15,527: 0% tax
-
-# 15,528 — 42,707: 15% tax
-
-# 42,708 — 132,406: 25% tax
-
-# 132,407 and more: 28% tax
-
-# The tax for 99999 is 25%. That is 25000 dollars!
-
-num = int(input())
-if num <= 15527:
-  k = (0)
-elif num >= 15528 and num <= 42707:
-  k = int(0,15)
-
-g = num * k
-proz = k * 100
-print("The tax for", num,"is",proz ,". That is",g,"dollars")
