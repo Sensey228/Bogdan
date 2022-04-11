277b0ab4df03c3961ff8c4f8dbb50a2f5a223cb3
Bogdan132 <39145828+Sensey228@users.noreply.github.com>
2022-04-11 00:31:22 +0000

    task 4

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-a = input()
-b = input()
-c = int(input())
+# Write a program that calculates the length of a word from 
+# the input and prints it out together 
+# with the word in the format:
 
-print(a,"(dir.", b ,") came out in" , c )
-# Fight Club (dir. David Fincher) came out in 1999
+# "word has N letters"
+
+# There will always be more than one letter in the word.
+
+# Sample Input 1:
+# serendipity
+
+# Sample Output 1:
+# serendipity has 11 letters
+
+word = input()
+len_ = len(word)
+print(word,"has",len_,"letters")
