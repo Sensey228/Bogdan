55448dd4e4166c991d60aa96cbf8c7df6be77047
Bogdan132 <39145828+Sensey228@users.noreply.github.com>
2022-04-11 01:23:53 +0000

    2 Слайсинг(пример)

--- main.py
+++ main.py
@@ -4,3 +4,19 @@ print(nums[2:5])
 name = 'Python is not a snake'
 print(name[::-1])
 
+nums = [n * 2 for n in range(1, 21)]
+print(nums)
+print(nums[2:7])
+
+str1 = 'Python is not just a mere snake'
+print(str1[10:18])
+
+print(str1[::-1])
+print(str1[7::])
+
+new_str = str1[:]
+print(new_str)
+
+my_list = ['Java', 'Python', 'Kotlin']
+print(my_list[:2])
+print(my_list[:99999999])
