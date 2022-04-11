431208281207cec0c35f15afe527fd7b7422e8e0
Bogdan132 <39145828+Sensey228@users.noreply.github.com>
2022-04-11 00:19:50 +0000

    task 2

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-pi = 3.141592653589793
-# Write a code that rounds the number pi to 5 decimal places and prints it out.
-# Use % formatter.
-print("{:.5f}".format(pi)) 
+num = float(input())
+ab = int(input())
+print(f'%.{ab}f' % (num))
