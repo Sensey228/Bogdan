d752ffdec0c8648669b5195c196da162aded1dc1
Bogdan132 <39145828+Sensey228@users.noreply.github.com>
2022-04-11 23:17:33 +0000

    Домино 2

--- main.py
+++ main.py
@@ -6,15 +6,31 @@ for i in range(7):
     stock.append([i, j])
 
 stock = [[i, j] for i in range(7) for j in range(i, 7)]
-print(stock)
+
+player =[]
+comp = []
+
+
+while len(stock) > 21:
+  x = random.randint(1, len(stock))
+  player.append(stock[x])
+  stock.pop(x)
+while len(stock) > 14:
+  x = random.randint(1, len(stock))
+  comp.append(stock[x])
+  stock.pop(x)
+
+print(stock)  
 print()
-
-
-
-player = (random.choices(stock, k=7))
 print(player)
 print()
-comp = (random.choices(stock, k=7))
 print(comp)
+
+
+# player = (random.choices(stock, k=7))
+# print(player)
+# print()
+# comp = (random.choices(stock, k=7))
+# print(comp)
 
 
