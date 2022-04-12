e9567a49b07aaddb469d08fc803676c39ef6b0d2
Bogdan132 <39145828+Sensey228@users.noreply.github.com>
2022-04-12 00:46:28 +0000

    Domino(почти, часть удалена)

--- main.py
+++ main.py
@@ -1,30 +1,65 @@
 import random
+dubl = 0
+while dubl != 1:
 
-stock = []
-for i in range(7):
-  for j in range(i, 7):
-    stock.append([i, j])
+  stock = []
+  for i in range(7):
+    for j in range(i, 7):
+      stock.append([i, j])
+  stock = [[i, j] for i in range(7) for j in range(i, 7)]
 
-stock = [[i, j] for i in range(7) for j in range(i, 7)]
+  player =[]
+  comp = []
 
-player =[]
-comp = []
+  i = 0
+  for i in range(7):
+    x = random.randint(1, len(stock)-1)
+    player.append(stock[x])
+    stock.pop(x)
+    
+  for i in range(7):
+    x = random.randint(1, len(stock)-1)
+    comp.append(stock[x])
+    stock.pop(x)
 
+  status_p = 0
+  status_c = 0
 
-while len(stock) > 21:
-  x = random.randint(1, len(stock))
-  player.append(stock[x])
-  stock.pop(x)
-while len(stock) > 14:
-  x = random.randint(1, len(stock))
-  comp.append(stock[x])
-  stock.pop(x)
+  for i in range(7):
+    summa = sum(player[i])
+    if summa == 10:
+      status_p = 1
+    elif summa == 12:
+      status_p = 2
+    print(status_p)
 
-print(stock)  
-print()
-print(player)
-print()
-print(comp)
+  print()
+  for i in range(7):
+    summa = sum(comp[i])
+    if summa == 10:
+      status_c = 1
+    elif summa == 12:
+      status_c = 2
+    print(status_c)
+  if status_c and status_p == 0:
+    dubl = 1
+snake = 0
+
+if status_c or status_p == 1:
+  snake = [[5, 5]]
+if status_c or status_p == 2:
+  snake = [[6, 6]]
+if status_p > status_c:
+  status = "player"
+if status_p < status_c:
+  status = "computer"
+  
+print("Stock pieces:", stock)
+print("Computer pieces:", comp)
+print("Player pieces:", player)
+print("Domino snake:", snake)
+print("Status:", status)
+
 
 
 # player = (random.choices(stock, k=7))
