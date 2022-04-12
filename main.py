5984bcd288e4187ab8069630acaeba0b98c57366
Bogdan132 <39145828+Sensey228@users.noreply.github.com>
2022-04-12 23:45:08 +0000

    2.7

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import random
+nums = [0, 1, 1, 2, 3, 5, 8]
 
-my_list = []
-list = []
-for i in range(100):
-  x = random.randint(0, 10000)
-  my_list.append(x)
 
-for i in range(100):
-  if my_list[i] % 5 == 0:
-    list.append(my_list[i])
-print(list)
+print(nums[0:-1])
+print(nums[0::1])
+print(nums[:])
+print(nums[1:])
+
+
