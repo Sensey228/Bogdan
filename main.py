5e2f91ad7715857b5ad30af19f50f3e0cfb88720
Bogdan132 <39145828+Sensey228@users.noreply.github.com>
2022-04-13 00:18:39 +0000

    3.3

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-time = int(input())
-
-for1 = time // 17
-for2 = time // 13
-
-if time % 17 != 0:
-  for1 = time // 17 + 1
-  
-if time % 13 != 0:
-  for2 = time // 13 + 1
-
-k = for1 + for2
-for i in range(for1):
-  for b in range(for2):
-      if 13 * b == 17 * i:
-        k = k - 1
-print(k)
-
-
+a, b = (int(i) for i in input().split())
+pod = b
+det = a
+k = 1
+while k > 0:
+  if pod - det > 0:
+    pod = pod - det
+  else:
+    print(det - pod)
+    k = 0
