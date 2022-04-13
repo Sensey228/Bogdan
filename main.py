fdf87f4fa0e55ae6d7d29bbe4c9416a13ec43dca
Bogdan132 <39145828+Sensey228@users.noreply.github.com>
2022-04-13 00:09:01 +0000

    3.2

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-a, b = (int(i) for i in input().split())
-N1 = a + b
-N2 = a - b
-print(N1 * N2)
+time = int(input())
+
+for1 = time // 17
+for2 = time // 13
+
+if time % 17 != 0:
+  for1 = time // 17 + 1
+  
+if time % 13 != 0:
+  for2 = time // 13 + 1
+
+k = for1 + for2
+for i in range(for1):
+  for b in range(for2):
+      if 13 * b == 17 * i:
+        k = k - 1
+print(k)
+
+
