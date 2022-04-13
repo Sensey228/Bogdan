0a245dbd43bd8a09d4d03516e8bdcd9b006bec38
Bogdan132 <39145828+Sensey228@users.noreply.github.com>
2022-04-13 01:28:36 +0000

    6.2

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from time import perf_counter
 num = int(input())
 num = num 
-
+time = 1000000000
 for i in range(1, num):
   if num % i == 0:
     print(i)
 print(num)
+x = perf_counter()
+print(x)
