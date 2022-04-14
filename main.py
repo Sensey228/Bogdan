7980a4d2ab9ae418b6a947807e51504a6a59287b
Bogdan132 <39145828+Sensey228@users.noreply.github.com>
2022-04-14 00:52:51 +0000

    Domino

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import randint, shuffle
+import random
 
 stock = [[i, j] for i in range(7) for j in range (i, 7)]
 doubles = stock[:-4:-2]
@@ -26,16 +27,77 @@ Computer pieces: {}\n\
    Domino snake: [{}]\n\
          Status: {}'.\
             format(stock, computer, player, snake, next_one))
-print(70 * "=")
-print("Stock size:", len(stock))
-print("Computer pieces:", len(computer))
-print(snake)
-print()
-print("Your pices:")
-for i in range(len(player)):
-  print(i+1,":",player[i])
+
+winner = 0
+while winner == 0:
+  
+  print(70 * "=")
+  print("Stock size:", len(stock))
+  print("Computer pieces:", len(computer))
+  print(snake)
   print()
-if (next_one) == "computer":
-  print("Status: Computer is about to make a move. Press Enter to continue...")
-else:
-  print("Status: It's your turn to make a move. Enter your command.")
+  print("Your pices:")
+  for i in range(len(player)):
+    print(i+1,":",player[i])
+    print()
+  if (next_one) == "computer":
+    print("Status: Computer is about to make a move. Press Enter to continue...")
+  else:
+    print("Status: It's your turn to make a move. Enter your command.")
+  
+
+  if next_one == "player":
+    try:
+      kost = int(input())
+    except ValueError:
+      print("Только числовые значения")
+      kost = int(input())
+      
+    if kost == 0:
+      stockr = random.randint(0, len(stock))
+      player.append(stock[stockr])
+    if kost > 0:
+      kost = kost - 1
+      snake = snake + player[kost]
+      player.pop(kost)
+      print(snake)
+    if kost < 0:
+      kost = kost * -1
+      kost = kost - 1
+      snake = player[kost] + snake
+      player.pop(kost)
+      print(snake)
+    
+    
+      
+  if next_one == "computer":
+    kost = input()
+    comp_rev = len(computer) * -1
+    kost = random.randint(comp_rev, len(computer))
+    if kost == 0:
+      stockr = random.randint(0, len(stock))
+      computer.append(stock[stockr])
+    if kost > 0:
+      kost = kost - 1
+      snake = snake + computer[kost]
+      computer.pop(kost)
+      print(snake)
+      
+    if kost < 0:
+      kost = kost * -1
+      kost = kost - 1
+      snake = computer[kost] + snake
+      computer.pop(kost)
+      print(snake)
+  if len(player) or len(computer) == 0:
+    winner = 0
+
+  if next_one == "computer":
+    next_one = "player"
+  else:
+    next_one = "computer"
+    
+if len(player) == 0:
+  print("Игрок победил!")
+if len(computer) == 0:
+  print("Компьютер победил!")
